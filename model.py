1e17a12558ae6f705d636074d295de161534e6ef
damion <damionfan@163.com>
2020-02-06 11:33:55 +0800

    first commit

---
+++ model.py
@@ -0,0 +1,8 @@
+# encoding: utf-8
+# Author    : damionfan@163.com
+# Datetime  : 2020/2/5 11:36
+# User      : Damion Fan
+# Product   : PyCharm
+# Project   : gcn
+# File      : model.py
+# explain   : 文件说明
---
+++ test.py
@@ -0,0 +1,16 @@
+# encoding: utf-8
+# Author    : damionfan@163.com
+# Datetime  : 2020/2/5 15:51
+# User      : Damion Fan
+# Product   : PyCharm
+# Project   : gcn
+# File      : test.py
+# explain   : 文件说明
+
+import numpy as np
+
+a = np.array([1,2,3])
+b = np.array([1,2,3])
+
+print(a.T <= a)
+
