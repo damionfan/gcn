c80992e30eeb0de66d8898497beb898277727ae5
damion <damionfan@163.com>
2020-02-06 13:06:36 +0800

    update gcn code

--- train.py
+++ train.py
@@ -6,3 +6,109 @@
 # Project   : gcn
 # File      : train.py
 # explain   : 文件说明
+
+import torch
+import torch.nn as nn
+from torch.autograd import Variable
+import torch.nn.functional as F
+import torch.optim as optim
+
+import time
+import argparse
+import numpy as np
+
+from model import GCN
+from utlis import load_data, accuracy
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--no_cuda', action='store_true', default=False,
+                    help='Disables CUDA training.')
+parser.add_argument('--seed', type=int, default=42, help='Random seed.')
+parser.add_argument('--epochs', type=int, default=200,
+                    help='Number of epochs to train.')
+parser.add_argument('--lr', type=float, default=.01, help='Initial learning rate.')
+parser.add_argument('--weight_decay', type=float, default=5e-4,
+                    help='Weight decay ')
+parser.add_argument('--hidden', type=int, default=16,
+                    help='Number of hidden layer.')
+parser.add_argument('--dropout', type=float, default=.5,
+                    help='Dropout rate')
+
+args = parser.parse_args()
+
+args.cuda = args.no_cuda
+np.random.seed(args.seed)
+torch.manual_seed(args.seed)
+
+
+class GCN_Net(torch.nn.Module):
+    def __init__(self, nfeat, nhid, nclass, dropout):
+        super(GCN_Net, self).__init__()
+
+        self.gc1 = GCN(nfeat, nhid)
+        self.gc2 = GCN(nhid, nclass)
+        self.dropout = dropout
+
+    def forward(self, x, adj):
+        x = F.relu(self.gc1(x, adj))
+        x = F.dropout(x, self.dropout, training=self.training)
+        x = self.gc2(x, adj)
+        return F.log_softmax(x, dim=1)
+
+
+# Load data
+
+data_set = 'cora'
+adj, features, labels, idx_train, idx_val, idx_test = load_data(data_set)
+
+# Model and optimizer
+
+model = GCN_Net(nfeat=features.shape[1],
+                nhid=args.hidden,
+                nclass=labels.max().item() + 1,
+                dropout=args.dropout)
+optimizer = optim.Adam(model.parameters(),
+                       lr=args.lr,
+                       weight_decay=args.weight_decay)
+
+def train(epoch):
+    t = time.time()
+    model.train()
+    optimizer.zero_grad()
+
+    output = model(features, adj)
+    loss_train = F.nll_loss(output[idx_train], labels[idx_train])
+    acc_train = accuracy(output[idx_train], labels[idx_train])
+
+    loss_train.backward()
+    optimizer.step()
+
+    loss_val = F.nll_loss(output[idx_val], labels[idx_val])
+    acc_val = accuracy(output[idx_val], labels[idx_val])
+
+    print('Epoch: {:04d}'.format(epoch + 1),
+          'loss_train: {:.4f}'.format(loss_train.item()),
+          'acc_train: {:.4f}'.format(acc_train.item()),
+          'loss_val: {:.4f}'.format(loss_val.item()),
+          'acc_val: {:.4f}'.format(acc_val.item()),
+          'time: {:.4f}s'.format(time.time() - t))
+
+def test():
+    model.eval()
+    output = model(features, adj)
+    loss_test = F.nll_loss(output[idx_test], labels[idx_test])
+    acc_test = accuracy(output[idx_test], labels[idx_test])
+    print("Test set results:",
+          "loss= {:.4f}".format(loss_test.item()),
+          "accuracy= {:.4f}".format(acc_test.item()))
+
+
+# Train model
+t_total = time.time()
+for epoch in range(args.epochs):
+    train(epoch)
+print("Optimization Finished!")
+print("Total time elapsed: {:.4f}s".format(time.time() - t_total))
+
+# Testing
+test()
